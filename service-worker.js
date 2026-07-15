da7aac8cc3327bdee6845f8dddc73ef0029058b4
Max <maxime.coic@gmail.com>
2026-07-15 14:55:37 +0200

    Corrige la bande visible en bas de l'app en mode standalone iOS (pin html/body au viewport)

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
    installed on the iOS home screen.
    ========================================================================== */
 
-var VERSION = 'v2';
+var VERSION = 'v3';
 var SHELL_CACHE = 'fp-shell-' + VERSION;
 var DATA_CACHE = 'fp-data-' + VERSION;
 var TILE_CACHE = 'fp-tiles-' + VERSION;
